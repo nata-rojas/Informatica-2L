7fd072c1706040fdc3f7630fd84f59bd296a258e
nata-rojas <naa.rojas456@gmail.com>
2021-03-16 22:24:13 -0500

    aplicación tareas vol 2

---
+++ Aplicación tareas 2/tabla.js
@@ -0,0 +1,33 @@
+let tareas = JSON.parse(localStorage.getItem("tareas"));
+if (tareas == null) tareas = [];
+
+let tabla = document.querySelector('#tareas')
+
+function llenarTabla() { 
+    let contenidoTabla ='<tr><th>Materia</th><th>Descripción</th><th>Fecha</th></tr>'
+    for (tarea of tareas) {
+        contenidoTabla = contenidoTabla + `<tr><td>${tarea.materia}</td><td>${tarea.descripción}</td><td>${tarea.fecha}</td></tr>`
+    }
+    tabla.innerHTML = contenidoTabla;
+}
+
+let form = document.querySelector('#formulario')
+
+function añadirTarea () { 
+    let materiaNueva = document.querySelector("input[name=Materia]").value
+    let descripciónNueva = document.querySelector("input[name=Descripción]").value
+    let fechaNueva = document.querySelector("input[name=Fecha]").value
+     
+    let tareaNueva = {materia:materiaNueva, descripción:descripciónNueva, fecha:fechaNueva}
+    console.log("Ahora voy a añadir la siguiente tarea");
+    console.log(tareaNueva)
+    
+    tareas.push(tareaNueva)
+    llenarTabla();
+    localStorage.setItem("tareas", JSON.stringify(tareas))
+} 
+
+
+form.onsubmit = añadirTarea
+
+llenarTabla()
